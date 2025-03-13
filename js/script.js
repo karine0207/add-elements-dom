1c0bb4bd528913654e12a5de37141649d21a30e9
Karine gil <karine-07@hotmail.es>
2025-03-13 13:04:45 +0100

    challege 2 hecho

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
  // Aquí tu código
+
+
+  
+  const agregados = document.getElementById("agregar");
+  agregados.addEventListener("click",insertar);
+
+  function insertar() {
+    
+
+    const primerRango = document.getElementById("lista");
+    const segundoRango = document.createElement("li");
+
+    
+    primerRango.appendChild(segundoRango).insertAdjacentHTML("afterbegin",prompt("Introducir Datos"));
+  }
+
+
+
+   
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
